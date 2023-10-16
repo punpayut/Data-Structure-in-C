ea28296283ff14565624a104b3ff8d5f1a18a63c
Payut Charoensri <61724549+punpayut@users.noreply.github.com>
2023-10-16 15:46:00 +0700

    array.c

--- src/array.c
+++ src/array.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#define SIZE 5
 
+int main() {
+  int arr[SIZE] = {2, 4, 6, 8, 10};
+  int i;
+
+  for (i = 0; i < SIZE; i++) {
+    printf("%d ", arr[i]);
+  }
+
+  return 0;
+}
